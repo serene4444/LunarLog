8703bb5af488ae4a6a3f11a16d42c7c7b9a61a9a
Plummer, Serene <sereneplummer@my.unt.edu>
2025-10-22 22:11:53 -0500

    main updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
-import './utils/storage'
+import './utils/storage'  // Add this line at the top
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
